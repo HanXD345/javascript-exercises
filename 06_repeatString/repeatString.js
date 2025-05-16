5db83582dd04814d9abe16925302c8d23c9b213d
HanXD345 <manpreet345@yahoo.com>
2025-05-16 16:19:32 -0400

    Complete Repeat String Exercise
    Completed the exercise by using loops to continuously add the given
    string by itself, and returning it in the function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, numberOfRepeats) {
 
+    if (numberOfRepeats < 0) {
+        return "ERROR"
+    }
+
+    fullStr = "";
+    for (let i = 0; i < numberOfRepeats; i++) {
+        fullStr += str;
+    }
+    return fullStr;
 };
 
 // Do not edit below this line
