6e79114f55bb7f6df1ad857220e1c2c9473c93be
HanXD345 <manpreet345@yahoo.com>
2025-05-16 17:13:02 -0400

    Complete JS exercise
    Completed the JS exercise by converting degrees in Celsius to Fahrenheit
    and vice versa via the given equation.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInFahrenheit) {
+  degreesInCelsius = (degreesInFahrenheit - 32) * (5/9);
+  return degreesInCelsius % 1 === 0 ? degreesInCelsius : Number(degreesInCelsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInCelsius) {
+  degreesInFahrenheit = (degreesInCelsius * (9/5)) + 32;
+  return degreesInFahrenheit % 1 === 0 ? degreesInFahrenheit : Number(degreesInFahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
