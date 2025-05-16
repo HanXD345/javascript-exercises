4bc657ca046d04964fa8b33dd4dce56a0591f27c
HanXD345 <manpreet345@yahoo.com>
2025-05-16 16:54:09 -0400

    Complete JS exercise
    Completed the exercise by checking to make sure the given numbers are
    valid, rounded integers, and adding all the numbers between the starting
    and ending points, inclusive.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    } else if (Math.round(start) - start !== 0 || Math.round(end) - end !== 0) {
+        return "ERROR";
+    } else if (start > end) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
 
+    let total = 0;
+
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
