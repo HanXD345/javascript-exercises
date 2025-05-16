085476e79efdbbfee0f81b823cb8c8e8d7be2713
HanXD345 <manpreet345@yahoo.com>
2025-05-16 17:02:01 -0400

    Completed JS exercise
    Completed the exercise by checking if the given year was a leap year
    based on the specifications given in the README.md file

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear = false;
+    if (year % 4 === 0) {
+        if (year % 400 === 0) {
+            leapYear = true
+        } else if (year % 100 === 0) {
+            leapYear = false
+        } else {
+            leapYear = true
+        }
+    };
 
+    return leapYear
 };
 
 // Do not edit below this line
