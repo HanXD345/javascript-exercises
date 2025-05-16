8b0576b828783abe2a05c6bf06d0ee9dcdf21e16
HanXD345 <manpreet345@yahoo.com>
2025-05-16 16:36:05 -0400

    Complete JS exercise
    Completed the JS exercise by continuously filtering through the array
    based on the number of arguments that are present in the function call.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    filteredArr = arr
+    for (let arg of args) {
+        filteredArr = filteredArr.filter((item) => item !== arg)
+    }
+    return filteredArr
 };
 
 // Do not edit below this line
